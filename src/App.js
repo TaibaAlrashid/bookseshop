240690de9a2f17f2ec1e2619c15434222605f195
TaibaAlrashid <talrashid@outlook.com>
2021-06-07 20:18:29 +0300

    bookberries store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import bkst1 from "./images/bkst1.jpg";
+import products from "./products";
 
 function App() {
+  const productsList = products.map((product) => (
+    <div key={product.id}>
+      <img alt={product.name} className="book" src={product.image}/>
+      <p className="text">{product.name}</p>
+      <p className="text">{product.price} KD</p>
+    </div>
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+      <h1 className="text">BookBerries</h1>
+      <h4 className="text">A store that helps you to find a good friend‟BOOK”.</h4>
+      <img alt="store" src={bkst1}/>
+    </div>
+    <div className="list">
+      {productsList}
+    </div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        id: 1,
+        name: "Letting Go", 
+        price: 6,
+        image:"https://i.pinimg.com/originals/e9/76/dc/e976dc844941b80cb8b86f078a4e74c1.png",
+    },
+    {
+        id: 2,
+        name: "The Power Of Now",
+        price: 5,
+        image:"https://image.ebooks.com/previews/001/001168/001168805/001168805.jpg",
+    },
+    {
+        id: 3,
+        name: "The Monk Who Sold His Ferrari",
+        price: 5,
+        image:"https://www.asiabooks.com/media/catalog/product/cache/1/image/264x/17f82f742ffe127f42dca9de82fb58b1/9/7/9780061125898.jpg",
+    },
+    {
+        id: 4,
+        name: "Eat Pray Love",
+        price: 7,
+        image:"https://www.booktopia.com.au/covers/big/9780747585664/0000/eat-pray-love.jpg",
+    },
+];
+
+export default products;
