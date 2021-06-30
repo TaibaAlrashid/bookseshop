cd3886ffcb3bdac99fe8aececd4d1259cbc16256
TaibaAlrashid <talrashid@outlook.com>
2021-06-30 20:57:29 +0300

    refactoring

--- src/stores/bookStore.js
+++ src/stores/bookStore.js
@@ -12,35 +12,55 @@ class BookStore {
   fetchBooks = async () => {
     try {
       const response = await axios.get("http://localhost:8000/products");
-      this.books = response.data;
+      this.products = response.data;
+      console.log(this.products);
     } catch (error) {
       console.log("fetchBooks:", error);
     }
   };
 
-  deleteProduct = (productId) => {
-    const newProducts = this.products.filter(
-      (product) => product.id !== productId
-    );
-    this.products = newProducts;
+  deleteProduct = async (productId) => {
+    try {
+      await axios.delete(`http://localhost:8000/products/${productId}`);
+      const newProducts = this.products.filter(
+        (product) => product.id !== productId
+      );
+      this.products = newProducts;
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  createProduct = (newProduct) => {
-    newProduct.id = this.products.length + 1;
-    newProduct.slug = slugify(newProduct.name);
-    this.products.push(newProduct);
+  createProduct = async (newProduct) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/products",
+        newProduct
+      );
+      this.products.push(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  updateProduct = (updateProduct) => {
-    const product = this.products.find(
-      (product) => product.id === updateProduct
-    );
-    product.name = updateProduct.name;
-    product.price = updateProduct.price;
-    product.Description = updateProduct.Description;
-    product.image = updateProduct.image;
+  updateProduct = async (updateProduct) => {
+    try {
+      await axios.put(
+        `http://localhost:8000/products/${updateProduct.id}, updateProduct`
+      );
+      const product = this.products.find(
+        (product) => product.id === updateProduct.id
+      );
 
-    product.slug = slugify(updateProduct.name);
+      product.name = updateProduct.name;
+      product.price = updateProduct.price;
+      product.Description = updateProduct.Description;
+      product.image = updateProduct.image;
+
+      product.slug = slugify(updateProduct.name);
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
 
